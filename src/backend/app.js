e25a032772a0a9387cbb01817e9177cca495de4a
Joaquin Loberza <loberzajoaco@gmail.com>
2025-09-10 13:06:31 -0300

    Intialize server ehit express.

---
+++ src/backend/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import { ERROR } from 'sqlite3';
+
+//Creamos el proceso de express y el puerto para el servidor.
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Midleware para recibir y enviar JSON.
+app.use(express.json())
+
+//Creamos el servidor.
+try {
+    app.listen(PORT, () => {
+        console.log(`Servidor inciado en el puerto ${PORT}...`)
+    })
+} catch (err) {
+    throw new Error(`ERROR:\n  No se ha podido inciar el servidor... - ${err}`)
+}
