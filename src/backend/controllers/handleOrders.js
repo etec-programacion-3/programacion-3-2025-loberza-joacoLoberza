821714ec891f6104e72944ee4e524884ba1c4fd3
Joaquin Loberza <loberzajoaco@gmail.com>
2025-10-23 16:46:54 -0300

    CRUD Carrito y Ordenes (2)

--- src/backend/controllers/handleOrders.js
+++ src/backend/controllers/handleOrders.js
@@ -1,17 +1,65 @@
-import { Order, OrderItem } from '../database/models.js';
-import { Sequelize } from 'sequelize';
+import { Order, OrderItem, User } from '../database/models.js';
+import { Sequelize, Op, or } from 'sequelize';
 
 class getOrdersDTO {
-    constructor(req, res) {
-        
+    constructor(req) {
+        this.limit = req.query.limit ? req.query.limit : 5;
+        this.after = req.query.after;
+        this.order = req.query.order;
+    }
+
+    validateOrder() {
+        if (this.order !== 'ASC' && this.order !== 'DESC') {
+            throw new Error('Invalid request order.')
+        } else {
+            this.pagOrder = [['orderNumber', this.order]];
+        }
+    }
+
+    validateFilter() {
+        if (this.after < 0 || this.limit <= 0 || this.limit > 50) {
+            throw new Error('Invalid pagination querys.')
+        } else {
+            this.pagFilter = this.after ? { id:{ [Op.gt] : this.after } } : {}
+        }
     }
 }
 
 
 export const getAllOrders = async (req, res) => {
     try {
-        const orders = await Order.findAll();
-        
+        const getAllOrdersDTO = new getOrdersDTO(req);
+        getAllOrdersDTO.validateOrder()
+        getAllOrdersDTO.validateFilter()
+        const orders = await Order.findAll({
+            where: getAllOrdersDTO.pagFilter,
+            order: getAllOrdersDTO.pagOrder,
+            limit: Number(getAllOrdersDTO.limit),
+            include: {
+                model: User,
+                where: { name : req.payload.name },
+                required: true
+            }
+        });
+
+        res.status(200).json({
+            success: true,
+            message: 'ACK| Orders found successfully.',
+            orders,
+            nextCursor: products.length ? products[products.length - 1].id : null,
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: `ERROR| Internal server error:\n  ${err}`
+        })
+    }
+}
+
+export const getOrdersById = async (req, res) => {
+    try {
+        const getOrdersByIdDTO = new getOrdersDTO(req);
     } catch (err) {
 
     }
